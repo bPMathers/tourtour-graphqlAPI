b98b35ee65a0f3510cb2e21843d91df3dbd4be53
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-07-10 10:35:58 -0400

    -fix short password error

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,5 +1,6 @@
-const isValidPassword = (password) => {
-    return password.length >= 8 && !password.toLowerCase().includes('password')
-}
+// this file does not seem to be used anymore
+// const isValidPassword = (password) => {
+//     return password.length >= 8 && !password.toLowerCase().includes('password')
+// }
 
-export { isValidPassword }
+// export { isValidPassword }
